9eb60b600f5f6ee7134f9072ccd3014da97f9563
Ezhovnik <igrovojk55@gmail.com>
2026-04-12 21:09:43 +0300

    new lua functions

--- build/res/scripts/stdcmd.lua
+++ build/res/scripts/stdcmd.lua
@@ -51,10 +51,14 @@ console.add_command(
 )
 
 console.add_command(
-    "tp obj:sel=$obj.id x:num~pos.x y:num~pos.y z:num~pos.z",
-    "Teleport object",
+    "tp entity:sel=$entity.id x:num~pos.x y:num~pos.y z:num~pos.z",
+    "Teleport entity",
     function (args, kwargs)
-        player.set_pos(unpack(args))
+        local eid, x, y, z = unpack(args)
+        local entity = entities.get(eid)
+        if entity ~= nil then
+            entity.transform:set_pos({x, y, z})
+        end
     end
 )
 console.add_command(
@@ -128,5 +132,15 @@ console.add_command(
         end
 
         return uptime .. " (" .. formatted_uptime .. ")"
+    end
+)
+
+console.add_command(
+    "player.respawn player:sel=$obj.id",
+    "Respawn player entity",
+    function (args, kwargs)
+        local eid = entities.spawn("chromaforge:player", {player.get_pos(args[1])}):get_uid()
+        player.set_entity(args[1], eid)
+        return "Spawned new player entity #"..tostring(eid)
     end
 )
