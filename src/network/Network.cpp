283616baec6393bbabfdb7a3d852823c429c2038
Ezhovnik <igrovojk55@gmail.com>
2026-05-24 11:43:10 +0300

    network: work in progress

---
+++ src/network/Network.cpp
@@ -0,0 +1,106 @@
+#include <network/Network.h>
+
+#include <curl/curl.h>
+#include <stdexcept>
+
+#include <debug/Logger.h>
+
+using namespace network;
+
+static size_t write_callback(
+    char* ptr, size_t size, size_t nmemb, void* userdata
+) {
+    auto& buffer = *reinterpret_cast<std::vector<char>*>(userdata);
+    size_t psize = buffer.size();
+    buffer.resize(psize + size * nmemb);
+    std::memcpy(buffer.data() + psize, ptr, size * nmemb);
+    return size * nmemb;
+}
+
+class CurlHttp : public Http {
+    CURL* curl;
+
+    size_t totalUpload = 0;
+    size_t totalDownload = 0;
+public:
+    CurlHttp(CURL* curl) : curl(curl) {
+    }
+
+    virtual ~CurlHttp() {
+        curl_easy_cleanup(curl);
+    }
+
+    void get(
+        const std::string& url,
+        OnResponse onResponse,
+        OnReject onReject
+    ) override {
+        std::vector<char> buffer;
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        CURLcode res = curl_easy_perform(curl);
+        if (res == CURLE_OK) {
+            long size;
+            if (!curl_easy_getinfo(curl, CURLINFO_REQUEST_SIZE, &size)) {
+                totalUpload += size;
+            }
+            if (!curl_easy_getinfo(curl, CURLINFO_HEADER_SIZE, &size)) {
+                totalDownload += size;
+            }
+            totalDownload += buffer.size();
+            if (onResponse) {
+                onResponse(std::move(buffer));
+            }
+        } else {
+            auto message = curl_easy_strerror(res);
+            LOG_ERROR("{} ({})", message, url);
+            if (onReject) {
+                onReject(message);
+            }
+        }
+    }
+
+    size_t getTotalUpload() const override {
+        return totalUpload;
+    }
+
+    size_t getTotalDownload() const override {
+        return totalDownload;
+    }
+
+    static std::unique_ptr<CurlHttp> create() {
+        if (auto curl = curl_easy_init()) {
+            return std::make_unique<CurlHttp>(curl);
+        }
+        LOG_ERROR("Could not initialize cURL");
+        throw std::runtime_error("Could not initialzie cURL");
+    }
+};
+
+
+Network::Network(std::unique_ptr<Http> http) : http(std::move(http)) {
+}
+
+Network::~Network() = default;
+
+void Network::httpGet(
+    const std::string& url, OnResponse onResponse, OnReject onReject
+) {
+    http->get(url, onResponse, onReject);
+}
+
+size_t Network::getTotalUpload() const {
+    return http->getTotalUpload();
+}
+
+size_t Network::getTotalDownload() const {
+    return http->getTotalDownload();
+}
+
+std::unique_ptr<Network> Network::create(
+    const NetworkSettings& settings
+) {
+    auto http = CurlHttp::create();
+    return std::make_unique<Network>(std::move(http));
+}
---
+++ src/network/Network.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <functional>
+
+#include <typedefs.h>
+#include <settings.h>
+#include <util/Buffer.h>
+
+namespace network {
+    using OnResponse = std::function<void(std::vector<char>)>;
+    using OnReject = std::function<void(const char*)>;
+
+    class Http {
+    public:
+        virtual ~Http() {}
+
+        virtual void get(
+            const std::string& url,
+            OnResponse onResponse,
+            OnReject onReject=nullptr
+        ) = 0;
+
+        virtual size_t getTotalUpload() const = 0;
+        virtual size_t getTotalDownload() const = 0;
+    };
+
+    class Network {
+        std::unique_ptr<Http> http;
+    public:
+        Network(std::unique_ptr<Http> http);
+        ~Network();
+
+        void httpGet(
+            const std::string& url,
+            OnResponse onResponse,
+            OnReject onReject = nullptr
+        );
+
+        size_t getTotalUpload() const;
+        size_t getTotalDownload() const;
+
+        static std::unique_ptr<Network> create(
+            const NetworkSettings& settings
+        );
+    };
+}
