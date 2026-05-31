3ed55781919ba740a9a58e6f59c86fedc94e2e1d
Ezhovnik <igrovojk55@gmail.com>
2026-05-31 23:10:05 +0300

    lua parser

---
+++ src/coders/lua_parsing.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace lua {
+    struct Location {
+        int pos;
+        int lineStart;
+        int line;
+    };
+
+    enum class TokenTag {
+        Keyword,
+        Name,
+        Integer,
+        Number,
+        OpenBracket,
+        CloseBracket,
+        String,
+        Operator,
+        Comma,
+        Semicolon,
+        Unexpected
+    };
+
+    struct Token {
+        TokenTag tag;
+        std::string text;
+        Location start;
+        Location end;
+
+        Token(
+            TokenTag tag,
+            std::string text,
+            Location start,
+            Location end
+        ) : tag(tag),
+            text(std::move(text)),
+            start(std::move(start)),
+            end(std::move(end)) {
+        }
+    };
+
+    bool is_lua_keyword(std::string_view view);
+
+    std::vector<Token> tokenize(std::string_view file, std::string_view source);
+}
