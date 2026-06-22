17c5608904accff2fea90200896586f7b2f367bf
Ezhovnik <igrovojk55@gmail.com>
2026-06-22 11:41:26 +0300

    Inventory upd

--- src/items/ItemStack.h
+++ src/items/ItemStack.h
@@ -2,36 +2,46 @@
 
 #include <typedefs.h>
 #include <constants.h>
+#include <data/dv.h>
 
 class ContentIndices;
 
 class ItemStack {
     itemid_t item = ITEM_EMPTY;
     itemcount_t count = 0;
+    dv::value fields = nullptr;
 public:
     ItemStack() = default;
 
-    ItemStack(itemid_t item, itemcount_t count);
+    ItemStack(itemid_t item, itemcount_t count, dv::value data=nullptr);
 
     void set(const ItemStack& item);
+    void set(ItemStack&& item);
     void setCount(itemcount_t count);
+
+    void setField(std::string_view name, dv::value value);
+    dv::value* getField(const std::string& name) const;
 
     bool accepts(const ItemStack& item) const;
     void move(ItemStack& item, const ContentIndices& indices);
 
-    inline void clear() {
+    void clear() {
         set(ItemStack(0, 0));
     }
 
-    inline bool isEmpty() const {
+    bool isEmpty() const {
         return item == ITEM_EMPTY;
     }
 
-    inline itemid_t getItemId() const {
+    itemid_t getItemId() const {
         return item;
     }
 
-    inline itemcount_t getCount() const {
+    itemcount_t getCount() const {
         return count;
+    }
+
+    const dv::value& getFields() const {
+        return fields;
     }
 };
