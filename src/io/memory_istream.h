53198d178d89b0674d37afd361ea44709a86b1ba
Ezhovnik <igrovojk55@gmail.com>
2026-07-28 10:58:26 +0300

    Fix: memory_istream UB in constructor init order, add seekoff/seekpos overrides

--- src/io/memory_istream.h
+++ src/io/memory_istream.h
@@ -18,13 +18,36 @@ protected:
     int_type underflow() override {
         return traits_type::eof();
     }
+
+    pos_type seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which) override {
+        if (!(which & std::ios_base::in)) {
+            return pos_type(off_type(-1));
+        }
+        char* base = eback();
+        char* end = egptr();
+        char* pos;
+        switch (dir) {
+            case std::ios_base::beg: pos = base + off; break;
+            case std::ios_base::cur: pos = gptr() + off; break;
+            case std::ios_base::end: pos = end + off; break;
+            default: return pos_type(off_type(-1));
+        }
+        if (pos < base || pos > end) {
+            return pos_type(off_type(-1));
+        }
+        setg(base, pos, end);
+        return pos_type(pos - base);
+    }
+
+    pos_type seekpos(pos_type pos, std::ios_base::openmode which) override {
+        return seekoff(static_cast<off_type>(pos), std::ios_base::beg, which);
+    }
 private:
     util::Buffer<char> buffer;
 };
 
-class memory_istream : public std::istream {
+class memory_istream : private memory_streambuf, public std::istream {
 public:
-    explicit memory_istream(util::Buffer<char> buffer) : std::istream(&buf), buf(std::move(buffer)) {}
-private:
-    memory_streambuf buf;
+    explicit memory_istream(util::Buffer<char> buffer)
+        : memory_streambuf(std::move(buffer)), std::istream(this) {}
 };
