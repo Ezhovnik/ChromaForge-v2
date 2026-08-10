8fec5c6602b54da51bd5b51a04f398d39516b87c
Ezhovnik <igrovojk55@gmail.com>
2026-08-11 00:21:50 +0300

    fix lua syntax highlighting

--- src/coders/commons.h
+++ src/coders/commons.h
@@ -25,6 +25,10 @@ inline bool is_identifier_start(char c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_';
 }
 
+inline bool is_minimal_identifier_part(int c) {
+    return is_identifier_start(c) || is_digit(c);
+}
+
 inline bool is_json_identifier_start(int c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_' || c == '.';
 }
