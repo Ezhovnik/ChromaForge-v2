f1a718b96814d44ec5b5e4eb1e2955918226af07
Ezhovnik <igrovojk55@gmail.com>
2026-09-15 00:40:33 +0300

    heightmaps upd

--- src/util/functional_util.h
+++ src/util/functional_util.h
@@ -3,31 +3,29 @@
 #include <glm/glm.hpp>
 
 namespace util {
-    template<class T>
-    struct pow {
+    template<class T, T (*F)(T, T)>
+    struct binary_op {
         constexpr T operator()(T a, T b) const {
-            return glm::pow(a, b);
+            return F(a, b);
         }
     };
 
-    template<class T>
-    struct min {
-        constexpr T operator()(T a, T b) const {
-            return glm::min(a, b);
-        }
-    };
+    template<class T> using pow = binary_op<T, glm::pow>;
+    template<class T> using min = binary_op<T, glm::min>;
+    template<class T> using max = binary_op<T, glm::max>;
 
-    template<class T>
-    struct max {
-        constexpr T operator()(T a, T b) const {
-            return glm::max(a, b);
-        }
-    };
-
-    template<class T>
-    struct abs {
+    template<class T, T (*F)(T)>
+    struct unary_op {
         constexpr T operator()(T a) const {
-            return glm::abs(a);
+            return F(a);
         }
     };
+
+    template<typename T> using abs = unary_op<T, glm::abs>;
+    template<typename T> using floor = unary_op<T, glm::floor>;
+    template<typename T> using round = unary_op<T, glm::round>;
+    template<typename T> using ceil = unary_op<T, glm::ceil>;
+    template<typename T> using sin = unary_op<T, glm::sin>;
+    template<typename T> using cos = unary_op<T, glm::cos>;
+    template<typename T> using tan = unary_op<T, glm::tan>;
 }
